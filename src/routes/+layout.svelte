2d184332ad228c5609ab0676bb9f17bd486ee227
Rodrigo Couto <r@rodg.co>
2023-08-19 01:12:55 -0300

    Progressier install button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,6 +19,7 @@
 				</ul>
 			</nav>
 			<Settings />
+    <button class="progressier-install-button reset" data-icons="false" data-install="Install" data-installed="Installed"></button>
 		<button class="reset" type="button" on:click={() => conversation.reset()}>Reset</button>
 		</div>
 	</header>
